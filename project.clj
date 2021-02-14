c2ccebff215665ab49a67f8aae30d5b3b76176bd
Thomas Heller <th.heller@gmail.com>
2021-02-14 12:03:13 +0100

    bump 0.1.0

--- project.clj
+++ project.clj
@@ -6,6 +6,10 @@
   {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
    :url "https://www.eclipse.org/legal/epl-2.0/"}
 
+  :repositories
+  {"clojars" {:url "https://clojars.org/repo"
+              :sign-releases false}}
+
   :dependencies
   [[org.clojure/clojure "1.10.2"]
    [org.clojure/core.async "1.3.610"]
