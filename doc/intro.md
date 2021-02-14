9fb057ecb9cf155ea91bd2b9c8c5d3bb6385201c
Thomas Heller <th.heller@gmail.com>
2021-02-14 11:59:05 +0100

    extract from thheller/shadow-cljs

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to shadow-undertow
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ project.clj
@@ -0,0 +1,25 @@
+(defproject thheller/shadow-undertow "0.1.0"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+
+  :license
+  {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
+   :url "https://www.eclipse.org/legal/epl-2.0/"}
+
+  :dependencies
+  [[org.clojure/clojure "1.10.2"]
+   [org.clojure/core.async "1.3.610"]
+   [io.undertow/undertow-core "2.2.4.Final"]]
+
+  :source-paths
+  ["src/main"]
+
+  :java-source-paths
+  ["src/main"]
+
+  :javac-options
+  ["-target" "1.8"
+   "-source" "1.8"]
+
+  :test-paths
+  ["src/test"])
